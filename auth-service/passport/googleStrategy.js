a617154b4a824a456c5797ff9c8e432ebe2ca51d
Mohammad Fahimuddin <mohammadfahimuddin@Mohammads-Air.lan>
2025-06-13 00:49:25 +0530

    Implementation: OAuth2.0

---
+++ auth-service/passport/googleStrategy.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/Users');
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID : process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: process.env.GOOGLE_CALLBACK_URL
+        },
+
+        async (accessToken, refreshToken, profile, done) => {
+            try{
+                let user = await User.findOne({where: {googleId : profile.id}});
+
+                if(!user){
+                    user = await User.create({
+                        googleId: profile.id,
+                        username: profile.displayName,
+                        email: profile.emails[0].value,
+                    });
+                }
+                return done(null, user);
+            }catch(err){
+                return done(err, null);
+            }
+        }
+    )
+)
+
+passport.serializeUser((user, done)=>{
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done)=> {
+    const user = User.findByPk(id);
+    done(null, user);
+})
--- auth-service/routes/auth.js
+++ auth-service/routes/auth.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controller/authController');
+const passport = require('passport');
+const jwt = require('jwtwebtoken')
 
 router.post('/register', authController.register);
 
+router.get('/google', 
+    passport.authenticate('google', {scope:['profile', 'email']}));
+
+router.get('google/callback',
+    passport.authenticate('google', {failureRedirect:'/login-failure', session:false}),
+    (req, res) => {
+        const token = jwt.sign(
+            { id: req.user.id }, 
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        )
+    }
+)
 module.exports = router;
