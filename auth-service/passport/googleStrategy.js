229b9caba6d1d678a1a767e9d5eb208a3bf58a31
Mohammad Fahimuddin <mohammadfahimuddin@Mohammads-Air.lan>
2025-06-14 17:07:18 +0530

    Implemented Google Login

--- auth-service/passport/googleStrategy.js
+++ auth-service/passport/googleStrategy.js
@@ -7,7 +7,7 @@ passport.use(
         {
             clientID : process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: process.env.GOOGLE_CALLBACK_URL
+            callbackURL: "/api/auth/google/callback"
         },
 
         async (accessToken, refreshToken, profile, done) => {
--- auth-service/routes/auth.js
+++ auth-service/routes/auth.js
@@ -2,21 +2,21 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controller/authController');
 const passport = require('passport');
-const jwt = require('jwtwebtoken')
+const jwt = require('jsonwebtoken')
 
 router.post('/register', authController.register);
+
+router.post('/login', authController.login);
 
 router.get('/google', 
     passport.authenticate('google', {scope:['profile', 'email']}));
 
-router.get('google/callback',
-    passport.authenticate('google', {failureRedirect:'/login-failure', session:false}),
-    (req, res) => {
-        const token = jwt.sign(
-            { id: req.user.id }, 
-            process.env.JWT_SECRET,
-            { expiresIn: '1h' }
-        )
-    }
-)
+router.get('/google/callback',
+  passport.authenticate('google', { session: false, failureRedirect: '/' }),
+  (req, res) => {
+    const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    // Redirect to frontend or return token
+    res.json({ token, message: 'Google Login Successful' });
+  }
+);
 module.exports = router;
